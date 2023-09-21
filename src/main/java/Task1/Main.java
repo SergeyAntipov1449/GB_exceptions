9e650d114fcc467379f807636fdb6826b630ff9f
Sergey Antipov <antipovsergey84@gmail.com>
2023-09-21 21:56:39 +0300

    Initial commit

---
+++ src/main/java/Task1/Main.java
@@ -0,0 +1,17 @@
+package Task1;
+
+//Реализуйте метод, принимающий в качестве аргументов целочисленный массив и некоторый допустимый минимум.
+//Если длина массива меньше некоторого заданного минимума, метод возвращает -1, в качестве кода ошибки, иначе - длину массива.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkArrayLength(new int[]{1, 4, 6, 7, 123, 475, -2}, 5));
+
+    }
+
+    public static int checkArrayLength(int[] array, int min) {
+        if (array.length < min) {
+            return -1;
+        }
+        return array.length;
+    }
+}
---
+++ src/main/java/Task4/Task4.java
@@ -0,0 +1,36 @@
+package Task4;
+//Реализуйте метод checkArray(Integer[] arr), принимающий в качестве аргумента целочисленный одномерный массив.
+//        Метод должен пройтись по каждому элементу и проверить что он не равен null. Реализуйте следующую логику:
+//        1. Если в какой-то ячейке встретился null, то необходимо “оповестить” об этом пользователя
+//        2. Если null’ы встретились в нескольких ячейках, то идеально было бы все их “подсветить”
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task4 {
+    public static void main(String[] args) {
+        checkArray2(new Integer[]{15, 466, null, null, 47});
+    }
+
+    public static void checkArray1(Integer[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                throw new NullPointerException(String.format("В ячейке с индексом %s содержится null", i));
+            }
+        }
+        System.out.println("Значение null не обнаружено");
+    }
+
+    public static void checkArray2(Integer[] arr) {
+        List<Integer> inexes = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                inexes.add(i);
+            }
+        }
+        if (!inexes.isEmpty()) {
+            throw new NullPointerException(String.format("В ячейках с индексами %s содержится null", inexes));
+        }
+        System.out.println("Значение null не обнаружено");
+    }
+}
